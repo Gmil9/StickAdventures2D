7e7f0e97199bf4c948ddf5b45639c556734e0b6e
Garrett Miller <gmil2626@gmail.com>
2018-10-16 15:32:49 -0500

    Jump and 2D Camera
    Fixed jump mechanic - added double jump
    Added 2D cinemachine camera that follows the player

---
+++ StickAdventures2D/Assets/Scenes/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    [SerializeField] float speed;
+    [SerializeField] float moveInput;
+    [SerializeField] float jumpForce;
+    Rigidbody2D rb;
+
+    bool isGrounded;
+    [SerializeField] Transform GroundCheck;
+    [SerializeField] float checkRadius;
+    [SerializeField] LayerMask ground;
+
+    int extraJumps = 1;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, ground);
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+	}
+
+    void Update()
+    {
+        if(isGrounded == true){
+            extraJumps = 1;
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0){
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
